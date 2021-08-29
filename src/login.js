c0d3fcf74c899894e20d955ccef568fdf773c827
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2021-08-29 10:22:52 -0400

    Updated redirect URI

--- src/login.js
+++ src/login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=3161af22a7714180bef6dbfe9a6fec9f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-read-email%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing'
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=3161af22a7714180bef6dbfe9a6fec9f&response_type=code&redirect_uri=https://vaijyant.github.io/spotify-visualizer&scope=streaming%20user-read-email%20user-read-private%20user-read-email%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing'
 export default function Login() {
     return (
        <Container>
